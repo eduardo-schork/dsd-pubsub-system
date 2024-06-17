36f23a93c9b159ab2fcb8da0869e976a63fecdee
Eduardo Schork <eduardo.schork1814@gmail.com>
2024-06-16 22:57:57 -0300

    chore: general fixes

--- src/broker.js
+++ src/broker.js
@@ -1,6 +1,6 @@
 const WebSocket = require("ws");
 
-const wss = new WebSocket.Server({ port: 8080 });
+const wss = new WebSocket.Server({ port: 8080, host: "localhost" });
 
 const subscribers = {
     ENE3: [],
@@ -10,38 +10,49 @@ const subscribers = {
 wss.on("connection", (ws) => {
     ws.on("message", (message) => {
         const data = JSON.parse(message);
+
         console.log(`Received message: ${message}`);
 
-        if (data.type === "subscribe") {
-            if (subscribers[data.channel]) {
-                subscribers[data.channel].push(ws);
-                ws.send(JSON.stringify({ message: `Subscribed to ${data.channel}` }));
-                console.log(`Subscriber added to channel: ${data.channel}`);
-            } else {
-                ws.send(JSON.stringify({ error: `Channel ${data.channel} does not exist` }));
-                console.log(`Error: Channel ${data.channel} does not exist`);
-            }
-        } else if (data.type === "publish") {
-            if (subscribers[data.channel]) {
-                subscribers[data.channel].forEach((subscriber) => {
-                    if (subscriber.readyState === WebSocket.OPEN) {
-                        subscriber.send(
-                            JSON.stringify({ channel: data.channel, price: data.price })
-                        );
-                        console.log(
-                            `Replicating message to subscriber in channel: ${data.channel}`
-                        );
-                    }
-                });
-                console.log(`Message published to channel: ${data.channel}, Price: ${data.price}`);
-            }
+        const hasChannel = subscribers[data.channel];
+
+        switch (data.type) {
+            case "subscribe":
+                if (hasChannel) {
+                    hasChannel.push(ws);
+                    ws.send(JSON.stringify({ message: `Subscribed to ${data.channel}` }));
+                    console.log(`Subscriber added to channel: ${data.channel}`);
+                } else {
+                    ws.send(JSON.stringify({ error: `Channel ${data.channel} does not exist` }));
+                    console.log(`Error: Channel ${data.channel} does not exist`);
+                }
+                break;
+
+            case "publish":
+                if (subscribers[data.channel]) {
+                    subscribers[data.channel].forEach((subscriber) => {
+                        if (subscriber.readyState === WebSocket.OPEN) {
+                            subscriber.send(
+                                JSON.stringify({ channel: data.channel, price: data.price })
+                            );
+                            console.log(
+                                `Replicating message to subscriber in channel: ${data.channel}`
+                            );
+                        }
+                    });
+                    console.log(
+                        `Message published to channel: ${data.channel}, Price: ${data.price}`
+                    );
+                }
+                break;
         }
+        console.log("============================================");
     });
 
     ws.on("close", () => {
         for (const channel in subscribers) {
             subscribers[channel] = subscribers[channel].filter((subscriber) => subscriber !== ws);
             console.log(`Subscriber removed from channel: ${channel}`);
+            console.log("============================================");
         }
     });
 });
--- src/subscriber.js
+++ src/subscriber.js
@@ -3,7 +3,7 @@ const WebSocket = require("ws");
 const ws = new WebSocket("ws://localhost:8080");
 
 ws.on("open", () => {
-    const channel = process.argv[2]; // Pass the channel as an argument when running the subscriber
+    const channel = process.argv[2];
     if (channel) {
         ws.send(JSON.stringify({ type: "subscribe", channel: channel }));
         console.log(`Subscribed to channel: ${channel}`);
